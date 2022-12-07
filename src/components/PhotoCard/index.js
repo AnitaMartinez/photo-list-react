d4f123fa895799630561aa6fe44687c4bc37a772
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-07 18:12:11 +0100

    Likes with LocalStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLazyLoad } from '../../hooks/useLazyLoad'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
@@ -8,6 +8,31 @@ const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null)
   const { show } = useLazyLoad(ref)
+  const keyItemLocalStorage = `like-${id}`
+  const [like, setLike] = useState(() => {
+    // hacemos el try catch por si hay algún error al pedir el localStorage, por ejemplo, si hay navegación privada
+    try {
+      const like = window.localStorage.getItem(keyItemLocalStorage)
+      return like
+    } catch (error) {
+      return false
+    }
+  })
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(keyItemLocalStorage, value)
+      setLike(value)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleClick = () => {
+    setLocalStorage(!like)
+  }
+
+  const Icon = like ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={ref}>
@@ -19,8 +44,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
+            <Button onClick={handleClick}>
+              <Icon size='32px' /> {likes} likes!
             </Button>
           </>
         )
