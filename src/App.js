86d0a609ffeac31a0daf9ed9b05ad40af1b0f0aa
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-09 17:13:32 +0100

    PhotoCardWithQuery

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/LisfOfCategories'
 import { ListOfPhotoCards } from './containers/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </div>
-)
+export const App = () => {
+  const currentQueryStringNavigation = window.location.search
+  const urlParams = new window.URLSearchParams(currentQueryStringNavigation)
+  const detailIdNavigation = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailIdNavigation
+          ? <PhotoCardWithQuery id={detailIdNavigation} />
+          : (
+            <>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={1} />
+            </>
+            )
+      }
+
+    </div>
+  )
+}
