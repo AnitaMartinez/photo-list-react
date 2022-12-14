bcb38447c19530e0bb9fee845dc962a61f387888
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-14 14:39:31 +0100

    Button likes

---
+++ src/components/FavButton/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Button } from './styles'
+
+export const FavButton = ({ like, likes, onClick }) => {
+  const Icon = like ? MdFavorite : MdFavoriteBorder
+
+  return (
+    <Button onClick={onClick}>
+      <Icon size='32px' /> {likes} likes!
+    </Button>
+  )
+}
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ImgWrapper, Img, Article } from './styles'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,12 +11,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const { show, ref } = useNearScreen()
   const keyItemLocalStorage = `like-${id}`
   const [like, setLike] = useLocalStorage(keyItemLocalStorage, false)
-
-  const handleClick = () => {
-    setLike(!like)
-  }
-
-  const Icon = like ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={ref}>
@@ -27,9 +22,28 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                 <Img src={src} />
               </ImgWrapper>
             </a>
-            <Button onClick={handleClick}>
-              <Icon size='32px' /> {likes} likes!
-            </Button>
+
+            <ToggleLikeMutation>
+              {
+                (toggleLike) => {
+                  const handleFavClick = () => {
+                    !like && toggleLike({
+                      variables: {
+                        input: { id }
+                      }
+                    })
+                    setLike(!like)
+                  }
+                  return (
+                    <FavButton
+                      like={like}
+                      likes={likes}
+                      onClick={handleFavClick}
+                    />
+                  )
+                }
+              }
+            </ToggleLikeMutation>
           </>
         )
       }
