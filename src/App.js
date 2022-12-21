6fcad361238ff2f13d68311fb08db045a2f5facf
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-21 16:19:51 +0100

    Detail page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import { Home } from './pages/Home'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Detail } from './pages/Detail'
 
 export const App = () => {
-  const currentQueryStringNavigation = window.location.search
-  const urlParams = new window.URLSearchParams(currentQueryStringNavigation)
-  const detailIdNavigation = urlParams.get('detail')
-
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Logo />
-      {
-        detailIdNavigation
-          ? <PhotoCardWithQuery id={detailIdNavigation} />
-          : (
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/pet/:id' element={<Home />} />
-            </Routes>
-            )
-      }
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pet/:id' element={<Home />} />
+        <Route path='/detail/:detailId' element={<Detail />} />
+      </Routes>
     </BrowserRouter>
   )
 }
