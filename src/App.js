ff4f583a1d410831364231a14007bede69638ea4
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-21 16:30:14 +0100

    NavBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import { Detail } from './pages/Detail'
+import { NavBar } from './components/NavBar'
 
 export const App = () => {
   return (
@@ -15,6 +16,7 @@ export const App = () => {
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
       </Routes>
+      <NavBar />
     </BrowserRouter>
   )
 }
