63e555399cf74997e39e7e63d5e94b067ea49e60
AnitaMartinez <amartinezaguilar6@gmail.com>
2022-12-30 13:22:35 +0100

    Protected routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -15,7 +22,27 @@ export const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/pet/:id' element={<Home />} />
         <Route path='/detail/:detailId' element={<Detail />} />
+
       </Routes>
+      <UserLogged>
+        {
+          ({ isAuth }) => (
+            isAuth
+              ? (
+                <Routes>
+                  <Route path='/favs' element={<Favs />} />
+                  <Route path='/user' element={<User />} />
+                </Routes>
+                )
+              : (
+                <Routes>
+                  <Route path='/favs' element={<NotRegisteredUser />} />
+                  <Route path='/user' element={<NotRegisteredUser />} />
+                </Routes>
+                )
+          )
+        }
+      </UserLogged>
       <NavBar />
     </BrowserRouter>
   )
